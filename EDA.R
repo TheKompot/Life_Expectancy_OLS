234f296479b6f37194502a87ad264b849f709928
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2023-04-22 19:48:34 +0200

    imported data and selected columns

--- EDA.R
+++ EDA.R
@@ -1,3 +1,8 @@
+# Import data
 df <- read.table("data.csv",sep=';',header = TRUE)
 dim(df)
 colnames(df)
+
+# Select columns
+df <- df[,c('Life.expectancy','Alcohol','BMI','GDP','Total.expenditure','Schooling')]
+dim(df)
